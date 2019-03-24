2469d9d7bdc545768a8176d15bad2f55fc3a7235
Zakeria Ali <zak.fined@gmail.com>
2019-03-24 23:32:43 +0000

    Add initial route and home component

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style/index.scss";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
