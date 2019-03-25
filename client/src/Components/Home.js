aa1397880351f84d3383e12e8d5e83cb210beb5a
Zakeria Ali <zak.fined@gmail.com>
2019-03-25 02:30:26 +0000

    Add Header and Nav

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -2,7 +2,35 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return <div className="home" />;
+    return (
+      <div className="home">
+        <aside>
+          <img src="/images/boro.png" alt="" />
+        </aside>
+        <nav>
+          <ul>
+            <a href="#">
+              <li>Work</li>
+            </a>
+            <a href="#">
+              <li>About</li>
+            </a>
+            <a href="#">
+              <li>Careers</li>
+            </a>
+            <a href="#">
+              <li>Contact</li>
+            </a>
+          </ul>
+        </nav>
+        <header>
+          <h1>Creative agency with a passion for digital</h1>
+          <a href="#">
+            <button>View our work</button>
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 
