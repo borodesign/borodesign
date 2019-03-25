334de792dc2c2f8e779d8f06b64b90c4bc4347a1
Zakeria Ali <zak.fined@gmail.com>
2019-03-25 13:37:53 +0000

    Animate overlay

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import { TimelineLite, CSSPlugin, Power2 } from "gsap/TweenMax";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.myTween = null;
+
+    this.overlay = null;
+    this.heading = null;
+  }
+  componentDidMount() {
+    this.myTween = new TimelineLite();
+    this.myTween.to(this.overlay, 3, {
+      scaleX: 0.86,
+      scaleY: 0.8,
+      width: "106%",
+      height: "92vh",
+      delay: 0.9,
+      ease: Power2.easeInOut
+    });
+    this.myTween.to(this.overlay, 0.01, { autoAlpha: 0 }, "-=.9");
+  }
+
   render() {
     return (
       <div className="home">
+        <div ref={element => (this.overlay = element)} className="overlay" />
         <aside>
           <img src="/images/boro.png" alt="" />
         </aside>
@@ -24,7 +46,9 @@ class Home extends Component {
           </ul>
         </nav>
         <header>
-          <h1>Creative agency with a passion for digital</h1>
+          <h1 ref={element => (this.heading = element)}>
+            Digital agency with a passion for creativity
+          </h1>
           <a href="#">
             <button>View our work</button>
           </a>
